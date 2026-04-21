632ed66b5643140d845916ea2ad2ff0df0304358
AotoImaizumi <zumimi5@icloud.com>
2026-04-21 16:48:12 +0900

    2-4 implemented

--- 2_rna_secondary_structure/code.py
+++ 2_rna_secondary_structure/code.py
@@ -15,16 +15,17 @@ def enumerate_pairs(fastafile: str) -> List[Tuple[int, int]]:
     # 課題 2-1
     with open(fastafile) as f:
         result = []
-        for record in SeqIO.parse(f, "fasta"):
-            # id = record.id
-            # description = record.description
-            seq = record.seq
+        # 1つのときはread()、複数の時はparse()
+        record = SeqIO.read(f, "fasta")
+        # id = record.id
+        # description = record.description
+        seq = record.seq
 
-            for i in range(len(seq)-1):
-                supplement = supplement_RNA_base(seq[i])
-                for j in range(i+1, len(seq)):
-                    if(seq[j] == supplement):
-                        result.append((i+1,j+1))
+        for i in range(len(seq)-1):
+            supplement = supplement_RNA_base(seq[i])
+            for j in range(i+1, len(seq)):
+                if(seq[j] == supplement):
+                    result.append((i+1,j+1))
     return result
 
 def enumerate_possible_pairs(fastafile: str, min_distance: int=4) -> List[Tuple[int, int]]:
@@ -52,20 +53,30 @@ def enumerate_continuous_pairs(fastafile: str, min_distance: int=4, min_length: 
 
 def create_dotbracket_notation(fastafile: str, min_distance: int=4, min_length: int=2) -> str:
     # 課題 2-4
-    return ""
+    # 結合部分をbracketで、それ以外をdotで表記する記法
+    continuous_pairs = enumerate_continuous_pairs(fastafile, min_distance, min_length)
+    len_seq = len(SeqIO.read(fastafile, "fasta").seq)
+    result = list("." * len_seq) # 文字列はイミュータブルで置換できないのでリストに
+
+    for tp in continuous_pairs:
+        for i in range(len_seq):
+            if tp[0] <= (i+1) < tp[0]+tp[2] and result[i] == ".":
+                result[i] = "("
+            elif tp[1]-tp[2] < (i+1) <= tp[1] and result[i] == ".":
+                result[i] = ")"
+    return "".join(result)
 
 if __name__ == "__main__":
     filepath = "data/AUCGCCAU.fasta"
     # 課題 2-1
-    print("2-1")
     print(enumerate_pairs(filepath))
     # 課題 2-2
-    print("2-2")
     print(enumerate_possible_pairs(filepath))
     # 課題 2-3
-    print("2-3")
     print(enumerate_continuous_pairs(filepath, 4,2))
     # 課題 2-4
-    print(create_dotbracket_notation(filepath, 2))
+    print(create_dotbracket_notation(filepath, 4,2))
 
+
+# 実際の実装では呼び出しがネストしてると重くて動かないっぽい
 
